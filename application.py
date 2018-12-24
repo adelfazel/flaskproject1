2123ad7664e1bce4c06eb04a2141f1ff1b9397d6
Adelfazel <adel.fazel86@gmail.com>
2018-12-25 00:10:07 +1100

    adding even more stuff

--- application.py
+++ application.py
@@ -29,3 +29,17 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    return "To DO"
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
